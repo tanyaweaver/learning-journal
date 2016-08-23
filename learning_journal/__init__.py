3d7a6345ac0684849d0b324d068a2ec7a008ad4d
tanyaweaver <tanya83@uw.edu>
2016-08-23 12:46:37 -0700

    moved files one level up to the repo level

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -6,6 +6,7 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
+    config.add_jinja2_renderer('.html')
     config.include('.routes')
     config.include('.views')
     config.scan()
