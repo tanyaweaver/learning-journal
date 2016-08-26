ef9f7a72864b557ec2fdbb25fb4f9ded2d027d70
Tatiana Weaver <tanya83@uw.edu>
2016-08-25 21:52:56 -0700

    Revert "Revert "Step2""

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
     'pytest',  # includes virtualenv
     'pytest-cov',
-    'tox'
+    'pytest-watch',
+    'tox',
     ]
 
 setup(name='learning_journal',
